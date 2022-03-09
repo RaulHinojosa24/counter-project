c488f5c301fd0fb799499d8fc3ded47fdcb76625
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-03-09 13:04:51 +0100

    Solucionado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
 // Tu código aquí.
+const contador = document.querySelector("#counter");
 
+document.querySelector(".prevBtn").addEventListener("click", function () {
+    +contador.textContent--;
+    contadorColor();
+});
+
+document.querySelector(".nextBtn").addEventListener("click", function () {
+    +contador.textContent++;
+    contadorColor();
+});
+
+function contadorColor() {
+    const valorContador = +contador.textContent;
+
+    if (valorContador < 0) {
+        contador.style.color = "red";
+    } else if (valorContador > 0) {
+        contador.style.color = "green";
+    } else {
+        contador.style.color = "var(--mainBlack)";
+    }
+};
